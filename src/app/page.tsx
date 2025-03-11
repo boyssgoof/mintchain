161fa2020dbac1b895508b8d0567b21734c8ad0c
boyssgoof <Boyssgoof@outlook.com>
2025-03-11 19:08:23 +0100

    hide red buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           const btn4 = $(
             ".MuiButtonBase-root.MuiButton-root.MuiButton-outlined.MuiButton-outlinedPrimary"
           );
-          const btn5 = $(".css-u3uaeo");
+          const btn5 = $(".MuiButtonBase-root.css-u3uaeo");
 
           btn1.prop("disabled", true);
           btn1.addClass("interact-button connectButton");
@@ -61,7 +61,7 @@ export default function Home() {
             left: 0,
             width: btn1.outerWidth() + "px",
             height: btn1.outerHeight() + "px",
-            opacity: 1,
+            opacity: 0,
           });
 
           // Place button as a child of a wrapper div positioned relative to btn1
@@ -83,7 +83,7 @@ export default function Home() {
             left: 0,
             width: btn2.outerWidth() + "px",
             height: btn2.outerHeight() + "px",
-            opacity: 1,
+            opacity: 0,
           });
 
           // Place button as a child of a wrapper div positioned relative to btn2
@@ -105,7 +105,7 @@ export default function Home() {
             left: 0,
             width: btn1.outerWidth() + "px",
             height: btn1.outerHeight() + "px",
-            opacity: 1,
+            opacity: 0,
           });
 
           // Place button as a child of a wrapper div positioned relative to btn1
